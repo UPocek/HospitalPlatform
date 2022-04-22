5ad2faed6474d0faf63eec38ab5462dfa56c626d
Tamara Ilic <tamarailic11@gmail.com>
2022-04-22 21:08:32 +0200

    fix [back]: Fixed some bugs made by resolving conflict

--- APP/Models/MedicalRecord.cs
+++ APP/Models/MedicalRecord.cs
@@ -5,41 +5,37 @@ using System.Text.Json.Serialization;
 [BsonIgnoreExtraElements]
 public class MedicalRecord
 {
+    [BsonElement("height")]
+    [JsonPropertyName("height")]
+    public int height {get; set;}
 
-    [BsonIgnoreExtraElements]
-    public class MedicalRecord
-    {
-        [BsonElement("height")]
-        [JsonPropertyName("height")]
-        public int height {get; set;}
+    [BsonElement("weight")]
+    [JsonPropertyName("weight")]
+    public int weight {get; set;}
 
-        [BsonElement("weight")]
-        [JsonPropertyName("weight")]
-        public int weight {get; set;}
+    [BsonElement("bloodType")]
+    [JsonPropertyName("bloodType")]
+    public string bloodType {get; set;}
 
-        [BsonElement("bloodType")]
-        [JsonPropertyName("bloodType")]
-        public string bloodType {get; set;}
+    [BsonElement("diseases")]
+    [JsonPropertyName("diseases")]
+    public List<string> diseases {get; set;}
 
-        [BsonElement("diseases")]
-        [JsonPropertyName("diseases")]
-        public List<string> diseases {get; set;}
+    [BsonElement("alergies")]
+    [JsonPropertyName("alergies")]
+    public List<string> alergies {get; set;}
 
-        [BsonElement("alergies")]
-        [JsonPropertyName("alergies")]
-        public List<string> alergies {get; set;}
+    [BsonElement("drugs")]
+    [JsonPropertyName("drugs")]
+    public List<Prescription> prescriptions {get; set;}
 
-        [BsonElement("drugs")]
-        [JsonPropertyName("drugs")]
-        public List<Prescription> prescriptions {get; set;}
+    [BsonElement("examinations")]
+    [JsonPropertyName("examinations")]
+    public List<int> examinations {get; set;}
 
-        [BsonElement("examinations")]
-        [JsonPropertyName("examinations")]
-        public List<int> examinations {get; set;}
+    [BsonElement("medicalInstructioins")]
+    [JsonPropertyName("medicalInstructioins")]
+    public List<MedicalInstructioin> medicalInstructioins {get; set;} = null;
 
-        [BsonElement("medicalInstructioins")]
-        [JsonPropertyName("medicalInstructioins")]
-        public List<MedicalInstructioin> medicalInstructioins {get; set;} = null;
-    }
 
 }
